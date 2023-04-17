6f9bcb60e34901d573d3c0e2e033179239714bfb
Павел Трипольский <paveltripolskij@MacBook-Air-Pavel.local>
2023-04-17 17:15:06 +0400

    fixed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from '@/components/Navbar/Navbar'
 import AppRouter from '@/router/AppRouter'
+import '/src/index.css'
 
 function App() {
   return (
--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react'
 const AboutPage: FC = () => {
   return (
     <div>
-      <h1>Welcome to about page</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to about page</h1>
     </div>
   )
 }
