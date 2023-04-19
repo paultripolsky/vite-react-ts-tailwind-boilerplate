75c9a2b67eba96c51fd5af03985874e2d87ac3fb
Павел Трипольский <paveltripolskij@MacBook-Air-Pavel.local>
2023-04-19 23:04:06 +0400

    add readme and delete unused imports from vite.config.js

---
+++ README.md
@@ -0,0 +1,14 @@
+**Dev mode**
+<br>
+`yarn dev`
+<br>
+<br>
+**Build**
+<br>
+`yarn dev`
+<br>
+<br>
+**Build and preview**
+<br>
+1. `yarn buld`
+2. `yarn preview`
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {postcss} from "autoprefixer";
-import {AcceptedPlugin} from "postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
